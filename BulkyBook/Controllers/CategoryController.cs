137dff24a927557453e9a6c62d3830bda56e11ae
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-07-05 18:22:38 +0530

    Edit Category GET

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -38,5 +38,39 @@ namespace BulkyBook.Controllers
             }
             return View(obj);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            //var categoryFirst = _db.Categories.FirstOrDefault(c => c.Id==id);
+            //var categorySingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+            var category = _db.Categories.Find(id);
+            if (category == null) { 
+                return NotFound();
+            }
+            return View(category);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder can not exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
     }
 }
