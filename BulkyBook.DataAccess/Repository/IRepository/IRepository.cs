27e7e3d6d59ab2f0034ac049c9cd65b92eb477e9
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-07-06 19:05:59 +0530

    IRepository and Reposiroty implemented.

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T- category, order,
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        void Add(T item);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> items);
+    }
+}
