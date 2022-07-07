b130f5b87986368ebc5835d41e976a39f6708ab6
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-07-07 18:32:03 +0530

    migration done to CoverType table

--- BulkyBook/Program.cs
+++ BulkyBook/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
+    builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("BulkyBook")
     ));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
