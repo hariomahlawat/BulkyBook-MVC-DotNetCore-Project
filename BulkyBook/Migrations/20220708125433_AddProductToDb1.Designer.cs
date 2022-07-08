20702abe80b1d2927bf44813ea042846b184dde6
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-07-08 18:26:37 +0530

    Product model added. Migrated to database.

--- BulkyBook/Migrations/20220708125433_AddProductToDb1.Designer.cs
+++ BulkyBook/Migrations/20220708125433_AddProductToDb1.Designer.cs
@@ -4,6 +4,7 @@ using BulkyBook.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyBook.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220708125433_AddProductToDb1")]
+    partial class AddProductToDb1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
