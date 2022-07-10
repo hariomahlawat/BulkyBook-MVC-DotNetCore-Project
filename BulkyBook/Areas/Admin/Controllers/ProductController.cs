967872e93b32b02bc3b6f35aad3c2d364085314a
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-07-10 10:39:22 +0530

    Projections using Select - ProductController

--- BulkyBook/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace BulkyBook.Areas.Admin.Controllers
 {
@@ -20,50 +22,43 @@ namespace BulkyBook.Areas.Admin.Controllers
             IEnumerable<Category> objCategoryList = _unitOfWork.Category.GetAll();
             return View(objCategoryList);
         }
-        //GET
-        public IActionResult Create()
-        {
-            return View();
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
-        {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("Name", "The DisplayOrder can not exactly match the Name.");
-            }
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Category.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Category created successfully!";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
+        
 
         //GET
-        public IActionResult Edit(int? id)
+        public IActionResult Upsert(int? id)
         {
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                    u=> new SelectListItem 
+                    {
+                        Text = u.Name,
+                        Value = u.Id.ToString()
+                    });
+            IEnumerable<SelectListItem> CoverType = _unitOfWork.CoverType.GetAll().Select(
+                    u => new SelectListItem
+                    {
+                        Text = u.Name,
+                        Value = u.Id.ToString()
+                    });
+
+
             if (id == null || id == 0)
             {
-                return NotFound();
+                //create product
+
+                return View(product);
             }
-            var category = _unitOfWork.Category.GetFirstOrDefault(c => c.Id == id);
-            //var categorySingle = _db.Categories.SingleOrDefault(c => c.Id == id);
-            //var category = _db.Categories.Find(id);
-            if (category == null)
-            {
-                return NotFound();
+            else 
+            { 
+                //update product
             }
-            return View(category);
+
+            return View(product);
         }
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category obj)
+        public IActionResult Upsert(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
