9420b9fbdad93a0d3d11d316b9595a43625b79a5
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-07-12 19:29:14 +0530

    Render HTML in datatables. Edit and Delete buttons added in datatables.

--- BulkyBook/wwwroot/js/product.js
+++ BulkyBook/wwwroot/js/product.js
@@ -1,5 +1,4 @@
-﻿
-var dataTable;
+﻿var dataTable;
 
 $(document).ready(function () {
     loadDataTable();
@@ -17,6 +16,20 @@ function loadDataTable() {
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "15%" },
             { "data": "coverType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                           <div class="w-75 btn-group" role="group">
+                        <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2">
+                            <i class="fa-solid fa-pencil"></i> Edit</a>
+                        <a href="" class="btn btn-danger mx-2">
+                            <i class="fa-solid fa-trash "></i> Delete</a>
+                    </div>
+                    `
+                },
+                "width": "15%"
+            }
         ]
     });
 } 
